c5ff0688af30c945da15722d6bc8cfca23343c51
Naoki Ueda <orange.s067803@gmail.com>
2014-07-25 08:35:35 +0900

    Filter Entityを追加

---
+++ InboxList/Filter.h
@@ -0,0 +1,17 @@
+//
+//  Filter.h
+//  InboxList
+//
+//  Created by Naoki Ueda on 2014/07/24.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Filter : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+
+@end
---
+++ InboxList/Filter.m
@@ -0,0 +1,16 @@
+//
+//  Filter.m
+//  InboxList
+//
+//  Created by Naoki Ueda on 2014/07/24.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "Filter.h"
+
+
+@implementation Filter
+
+@dynamic name;
+
+@end
