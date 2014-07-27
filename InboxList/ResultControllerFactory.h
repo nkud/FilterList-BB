7dacc2dfb53b050c431d4dcc173a895d0236a473
Naoki Ueda <orange.s067803@gmail.com>
2014-07-27 11:58:46 +0900

    new ResultControllerFactory

---
+++ InboxList/ResultControllerFactory.h
@@ -0,0 +1,13 @@
+//
+//  ResultControllerFactory.h
+//  InboxList
+//
+//  Created by Naoki Ueda on 2014/07/27.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ResultControllerFactory : NSObject
+
+@end
---
+++ InboxList/ResultControllerFactory.m
@@ -0,0 +1,13 @@
+//
+//  ResultControllerFactory.m
+//  InboxList
+//
+//  Created by Naoki Ueda on 2014/07/27.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "ResultControllerFactory.h"
+
+@implementation ResultControllerFactory
+
+@end
