60364817e82e6537bab004df033493a9ad1aa525
Naoki Ueda <su104003@gmail.com>
2014-08-24 20:00:03 +0900

    タグ表示のバグを修正

---
+++ InboxList/TagSelectCell.m
@@ -0,0 +1,25 @@
+//
+//  TagSelectCell.m
+//  InboxList
+//
+//  Created by Naoki Ueda on 2014/08/24.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "TagSelectCell.h"
+
+@implementation TagSelectCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
