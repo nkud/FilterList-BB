f5b7b326af364cbf1f65334dcbf63770160b9a7e
Naoki Ueda <su104003@gmail.com>
2014-09-14 12:58:32 +0900

    ItemDetailViewController2作成

---
+++ InboxList/ItemDetailViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ItemDetailViewController2.h
+//  InboxList
+//
+//  Created by Naoki Ueda on 2014/09/10.
+//  Copyright (c) 2014 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemDetailViewController2 : UITableViewController
+
+@end
