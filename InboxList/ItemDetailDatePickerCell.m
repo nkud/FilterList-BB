600f7bebc0ee8e5c0801fbf9a5c7da00044ca4ab
Naoki Ueda <su104003@gmail.com>
2014-09-17 23:54:25 +0900

    ピッカーの値が変更された時の処理を追加

--- InboxList/ItemDetailDatePickerCell.m
+++ InboxList/ItemDetailDatePickerCell.m
@@ -3,19 +3,38 @@
 //  InboxList
 //
 //  Created by Naoki Ueda on 2014/09/14.
-//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//  Copyright (c) 2014 Naoki Ueda. All rights reserved.
 //
 
 #import "ItemDetailDatePickerCell.h"
 
 @implementation ItemDetailDatePickerCell
 
+/**
+ * @brief  初期化
+ */
 - (void)awakeFromNib
 {
   // Initialization code
-  
   // ピッカーの時間間隔を指定
   self.datePicker.minuteInterval = 10;
+  [self.datePicker addTarget:self
+                      action:@selector(pickerDidChangedValue:)
+            forControlEvents:UIControlEventValueChanged];
+}
+
+/**
+ * @brief  ピッカーの値が変更された時の処理
+ *
+ * @param datePicker ピッカー
+ */
+-(void)pickerDidChangedValue:(UIDatePicker *)datePicker
+{
+  NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+  [dateFormatter setDateFormat:@"yyyy/MM/dd hh:mm"];
+  NSString *dateString = [dateFormatter stringFromDate:datePicker.date];
+  
+  NSLog(@"%s %@", __FUNCTION__, dateString);
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
