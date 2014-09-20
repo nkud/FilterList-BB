de6fb4cab034d05b346a9666e4fc567721246cba
Naoki Ueda <su104003@gmail.com>
2014-09-20 16:20:43 +0900

    InputHeaderCellクラスを追加

---
+++ InboxList/InputHeaderCell.h
@@ -0,0 +1,13 @@
+//
+//  InputHeaderCell.h
+//  InboxList
+//
+//  Created by Naoki Ueda on 2014/09/20.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputHeaderCell : UITableViewCell
+
+@end
---
+++ InboxList/InputHeaderCell.m
@@ -0,0 +1,23 @@
+//
+//  InputHeaderCell.m
+//  InboxList
+//
+//  Created by Naoki Ueda on 2014/09/20.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "InputHeaderCell.h"
+
+@implementation InputHeaderCell
+
+- (void)awakeFromNib {
+  // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+  [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
