3c5a7416b4f63d05a80b65442d093065f3919788
Naoki Ueda <su104003@gmail.com>
2014-09-20 23:11:29 +0900

    new InputHeaderViewController class

---
+++ InboxList/InputHeaderViewController.h
@@ -0,0 +1,13 @@
+//
+//  InputHeaderViewController.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/09/20.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputHeaderViewController : UIViewController
+
+@end
---
+++ InboxList/InputHeaderViewController.m
@@ -0,0 +1,37 @@
+//
+//  InputHeaderViewController.m
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/09/20.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "InputHeaderViewController.h"
+
+@interface InputHeaderViewController ()
+
+@end
+
+@implementation InputHeaderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
