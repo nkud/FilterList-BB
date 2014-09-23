10df2ffe1c5ff68719c8f75d337a0e3a3c96d9ba
Naoki Ueda <su104003@gmail.com>
2014-09-23 13:15:04 +0900

    完了リストを作成

---
+++ InboxList/CompleteViewController.h
@@ -0,0 +1,13 @@
+//
+//  CompleteViewController.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/09/23.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompleteViewController : UIViewController
+
+@end
