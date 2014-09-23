3fe2214b5bc87de83cbee5b662bfff33d060dce9
Naoki Ueda <su104003@gmail.com>
2014-09-23 21:40:36 +0900

    new ListViewController class

---
+++ InboxList/ListViewController.h
@@ -0,0 +1,14 @@
+//
+//  ListViewController.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/09/23.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
---
+++ InboxList/ListViewController.m
@@ -0,0 +1,37 @@
+//
+//  ListViewController.m
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/09/23.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "ListViewController.h"
+
+@interface ListViewController ()
+
+@end
+
+@implementation ListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
