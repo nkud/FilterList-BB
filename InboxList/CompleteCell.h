ca5f10d078a8055fde5e87b56fcbe91e411c0276
Naoki Ueda <su104003@gmail.com>
2014-10-07 18:40:52 +0900

    完了リスト用のセルを追加

---
+++ InboxList/CompleteCell.h
@@ -0,0 +1,13 @@
+//
+//  CompleteCell.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/10/07.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompleteCell : UITableViewCell
+
+@end
---
+++ InboxList/CompleteCell.m
@@ -0,0 +1,23 @@
+//
+//  CompleteCell.m
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/10/07.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "CompleteCell.h"
+
+@implementation CompleteCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
