d7badf51bcf5044eec422eb33b38d1cdacd93542
Naoki Ueda <su104003@gmail.com>
2014-10-12 14:26:47 +0900

    フィルター入力画面を作成

---
+++ InboxList/InputFilterViewController2.h
@@ -0,0 +1,13 @@
+//
+//  InputFilterViewController2.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/10/12.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputFilterViewController2 : UIViewController
+
+@end
---
+++ InboxList/InputFilterViewController2.m
@@ -0,0 +1,37 @@
+//
+//  InputFilterViewController2.m
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/10/12.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "InputFilterViewController2.h"
+
+@interface InputFilterViewController2 ()
+
+@end
+
+@implementation InputFilterViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
