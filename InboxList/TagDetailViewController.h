35a19f03768f74b3c616658e54b00ead43ff4c68
Naoki Ueda <su104003@gmail.com>
2014-10-19 16:23:54 +0900

    タグ詳細ビューを新規に作成

---
+++ InboxList/TagDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TagDetailViewController.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/10/19.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagDetailViewController : UIViewController
+
+@end
---
+++ InboxList/TagDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  TagDetailViewController.m
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/10/19.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "TagDetailViewController.h"
+
+@interface TagDetailViewController ()
+
+@end
+
+@implementation TagDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
