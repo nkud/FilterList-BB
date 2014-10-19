2e7777319efe94b6e60d906f59276fa366fa7944
Naoki Ueda <su104003@gmail.com>
2014-10-19 16:46:50 +0900

    TagTitleCellを追加

---
+++ InboxList/TagTitleCell.m
@@ -0,0 +1,23 @@
+//
+//  TagTitleCell.m
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/10/19.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "TagTitleCell.h"
+
+@implementation TagTitleCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
