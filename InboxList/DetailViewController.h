bf71046a006e9cb500158adb99bdf73002a11d16
Naoki Ueda <su104003@gmail.com>
2014-10-22 04:01:27 +0900

    詳細ビューインターフェイスクラスを追加

---
+++ InboxList/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/10/22.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
