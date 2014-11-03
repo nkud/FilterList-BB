d2109de40865b8be2cb4be6a6ea09999f1cb18aa
Naoki Ueda <su104003@gmail.com>
2014-11-04 00:00:07 +0900

    ConfigViewControllerを新規作成する

---
+++ InboxList/ConfigViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigViewController.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/11/03.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface ConfigViewController : DetailViewController
+
+@end
