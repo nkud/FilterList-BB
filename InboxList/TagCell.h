0c0de35ee3e6202961f94b64636eb937353d4cf6
Naoki Ueda <su104003@gmail.com>
2014-12-02 00:54:06 +0900

    ---

--- InboxList/TagCell.h
+++ InboxList/TagCell.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 Naoki Ueda. All rights reserved.
 //
 
-// TODO: 普通に消す時にラベルが消えてしまう
-
 #import <UIKit/UIKit.h>
 
 /// タグのセル
@@ -16,8 +14,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *labelForTitle;
 
 @property (weak, nonatomic) IBOutlet UILabel *labelForItemSize;
-//@property (weak, nonatomic) IBOutlet UILabel *labelForOverDueItemsSize;
-//@property (weak, nonatomic) IBOutlet UILabel *labelForDueToTodayItemsSize;
 
 - (void)showItemSizeLabel:(BOOL)show;
 
