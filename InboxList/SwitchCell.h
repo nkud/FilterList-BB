dbc2dd05bc378cf19ca5f02e452c7c8ec197a93e
Naoki Ueda <su104003@gmail.com>
2014-12-06 11:07:53 +0900

    add SwitchCell.

---
+++ InboxList/SwitchCell.h
@@ -0,0 +1,13 @@
+//
+//  SwitchCell.h
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/12/06.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwitchCell : UITableViewCell
+
+@end
---
+++ InboxList/SwitchCell.m
@@ -0,0 +1,23 @@
+//
+//  SwitchCell.m
+//  FilterList
+//
+//  Created by Naoki Ueda on 2014/12/06.
+//  Copyright (c) 2014年 Naoki Ueda. All rights reserved.
+//
+
+#import "SwitchCell.h"
+
+@implementation SwitchCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
